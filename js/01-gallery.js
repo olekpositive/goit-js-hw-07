cf7082eebde2bb2011a858eb8a69e00e2959a9d4
Oleksandr Kihim <oleksandr.kigim@gmail.com>
2022-11-16 20:48:36 +0100

    public_tasks

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,33 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const instance = basicLightbox.create(`<img src="" alt=""/>`);
+const gallery = document.querySelector(".gallery");
+
+const images = galleryItems.map((image) =>
+    `<a class="gallery__link" href="${image.original}"><img class="gallery__image" 
+        src="${image.preview}" data-source="${image.original}" alt="${image.description}"/></a>`).join("");
+
+gallery.insertAdjacentHTML("afterbegin", images);
+
+gallery.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    const clickedElement = event.target;
+    if (clickedElement.nodeName !== "IMG") return;
+
+    const largeImage = clickedElement.dataset.source;
+    const alt = clickedElement.alt;
+    const modal = instance.element();
+    const modalImgage = modal.querySelector("IMG");
+    modalImgage.src = largeImage;
+    modalImgage.alt = alt;
+
+    instance.show();
+});
+
+gallery.addEventListener("keydown", (event) => {
+    if (event.key === "Escape") {
+        instance.close();
+}
+});
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,16 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+import { galleryItems } from './gallery-items.js';
+
+const gallery = document.querySelector(".gallery");
+
+const images = galleryItems.map((image) =>
+    `<a class="gallery__link" href="${image.original}"><img class="gallery__image" 
+        src="${image.preview}" data-source="${image.original}" alt="${image.description}"/></a>`).join("");
+
+gallery.insertAdjacentHTML("afterbegin", images);
+
+
+const lightbox = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250,
+});
